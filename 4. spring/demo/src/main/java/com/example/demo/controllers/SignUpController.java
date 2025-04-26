af14b6f4b10e1da24503603e3029c47f1d3b547f
danil-Itis <tron5268@mail.ru>
2025-04-26 14:01:59 +0300

    26.04
    email send

--- 4. spring/demo/src/main/java/com/example/demo/controllers/SignUpController.java
+++ 4. spring/demo/src/main/java/com/example/demo/controllers/SignUpController.java
@@ -2,10 +2,13 @@ package com.example.demo.controllers;
 
 import com.example.demo.dto.UserForm;
 import com.example.demo.services.SignUpService;
+import freemarker.template.TemplateException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.io.IOException;
 
 @Controller
 public class SignUpController {
@@ -19,7 +22,7 @@ public class SignUpController {
     }
 
     @PostMapping("/signUp")
-    public String signUp(UserForm form){
+    public String signUp(UserForm form) throws TemplateException, IOException {
         System.out.println("************");
         System.out.println(form.getEmail());
         System.out.println(form.getPassword());
--- 4. spring/demo/src/main/java/com/example/demo/services/SignUpService.java
+++ 4. spring/demo/src/main/java/com/example/demo/services/SignUpService.java
@@ -1,7 +1,10 @@
 package com.example.demo.services;
 
 import com.example.demo.dto.UserForm;
+import freemarker.template.TemplateException;
+
+import java.io.IOException;
 
 public interface SignUpService {
-    public void addUser(UserForm userForm);
+    public void addUser(UserForm userForm) throws TemplateException, IOException;
 }
